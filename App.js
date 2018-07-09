90a03a58f24e9ea78630b47f5888176041ed5493
mattpodolak <mpodola2@uwo.ca>
2018-07-09 18:49:29 -0400

    Removed firebase

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import MainActivity from './Modules/MainActivity' ;
 import SecondActivity from './Modules/SecondActivity' ;
 import MainDrawer from './Modules/MainDrawer' ;
 import MainModal from './Modules/MainModal' ;
+import * as firebase from 'firebase';
 
 import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
 
@@ -36,5 +37,35 @@ const RootStack = createStackNavigator(
 export default class App extends React.Component {
 	render() {
 		return <RootStack />;
-	}
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+  },
+  textInput: {
+    width: Dimensions.get('window').width - 30,
+    marginHorizontal: 15,
+    padding: 5,
+    borderRadius: 2,
+    borderWidth: 1,
+    borderColor: '#eee',
+    marginVertical: 15,
+    height: 50,
+    fontSize: 16,
+  },
+  loadingOverlay: {
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+//Exponent.registerRootComponent(App);
--- Modules/SecondActivity.js
+++ Modules/SecondActivity.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
- 
+
 class SecondActivity extends Component
 {
     
