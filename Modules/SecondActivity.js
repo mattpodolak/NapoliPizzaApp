90b81730abe76d3754a6e1e42396609fde148905
jtgomez <jason@enlightme.ca>
2018-07-10 19:48:02 -0400

    added toppings

--- Modules/SecondActivity.js
+++ Modules/SecondActivity.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, StatusBar, TouchableOpacity, Platform, Alert, ScrollView} from 'react-native';
+import {DrawerActions} from 'react-navigation';
+import { Ionicons as Icon } from '@expo/vector-icons';
+
+
+initialArr = [
+    {
+        id: 1,
+        price: 32.99,
+        name: "Mega Deal",
+        toppings: 3,
+        size: 'Medium',
+        addon: {
+            name: 'Upgrade to large',
+            price: 2
+
+        },
+        extras: {
+            wings: 10,
+            pop: 4,
+            dip: 2,
+            pasta: 'true'
+        }
+    }
+  ];
 
 class SecondActivity extends Component
 {
@@ -8,15 +32,60 @@ class SecondActivity extends Component
  {
     title: 'Cart',
  };
- 
- render()
- {
+
+calculatePrice(item){
+    console.log('calculating price...')
+    var total = 0;
+    total += item.price;
+    console.log(total);
+    // add on section
+    if (item.addon.price){
+        total += item.addon.price;
+        console.log(total);
+    }
+    // check size
+    if (item.size == 'Medium'){
+        total += 100;
+    }
+    console.log(total);
+    return total;
+}
+
+render()
+{   
     return(
-       <View style = { styles.MainContainer }>
- 
-          <Text style = { styles.ActivityNameTextCss }> This Is SecondActivity. </Text>
- 
-       </View>
+<View style={styles.container}>
+            <StatusBar barStyle="dark-content" />
+                <View style={styles.header}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            this.props.navigation.dispatch(DrawerActions.toggleDrawer());
+                        }}
+                    >
+                        <Icon name="md-menu" size={30} />
+                    </TouchableOpacity>
+                    {/* MAP CART */}
+                    <View>
+                        <ScrollView>
+                            {initialArr.map((buttonInfo) => {
+                                return (
+                                    <View key={buttonInfo.id}>
+
+                                        <Text style={styles.item}>
+                                            {buttonInfo.name}{'\n'}
+                                            Price: {buttonInfo.price}{'\n'}
+                                            Size: {buttonInfo.size}{'\n'}
+                                            Total Price: {this.calculatePrice(buttonInfo)}
+                                            
+                                        </Text>
+                                    </View>
+                                );
+                            })}
+                        </ScrollView>
+                    </View>
+                    
+                </View>
+        </View>
     );
  }
 }
@@ -37,6 +106,28 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: '#000',
      },
+
+     container: {
+		flex: 1,
+		paddingTop: StatusBar.currentHeight,
+		backgroundColor: '#FFF',
+    },
+    header: { 
+        padding: 15, 
+        paddingTop: Platform.OS === 'ios' ? 13 : 7, 
+        alignContent : 'center'
+    },    
+	item: {
+        fontSize: 10,
+        backgroundColor: 'dimgrey',
+        margin: 5,
+        textAlign: 'center',
+	},
+	price: {
+        fontSize: 10,
+        backgroundColor: 'lightgrey',
+        fontWeight: 'bold',
+	}, 
     
     });
  
