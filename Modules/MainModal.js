3144cea0966e7b47fbc59937abf915cc211c49ec
mattpodolak <mpodola2@uwo.ca>
2018-07-10 22:29:27 -0400

    Working on dynamic order form

--- Modules/MainModal.js
+++ Modules/MainModal.js
@@ -1,15 +1,95 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
 import t from 'tcomb-form-native';
 
-const custom_data = require('./structure/custom_json.json');
+var formStylesheet = require('./src/FormStyles.js');
 const Form = t.form.Form;
+const custom_data = require('./structure/custom_json.json');
 
+var all_toppings = [
+'Pepperoni', 
+'Ham', 
+'Bacon', 
+'Chicken', 
+'Italian Sausage', 
+'Cheddar', 
+'Parmesan', 
+'X-Cheese', 
+'Double Cheese', 
+'Feta', 
+'Mushrooms', 
+'Green Peppers', 
+'Green Olives', 
+'Black Olives', 
+'Hot Peppers', 
+'Red Peppers', 
+'Jalapenos', 
+'Broccoli', 
+'Onions', 
+'Pineapple', 
+'Tomatoes', 
+'Sun Dried Tomoatoes', 
+'Spinach', 
+'Thin Crust', 
+'Thick Crust', 
+'Whole Wheat', 
+'Extra Sauce', 
+'Chili Pepper', 
+'Fresh Garlic'
+];
+
+var all_pops = [
+'Coke',
+'Diet Coke',
+'Coke Zero',
+'Sprite',
+'Nestea',
+'Barqs Root Beer',
+'Canada Dry',
+'C-Plus',
+'Fresca',
+'Brio',
+'Water'
+];
+
+var all_dips = [
+  'Ranch',
+  'Garlic',
+  'Blue Cheese',
+  'Marinara',
+  'Cheddar Chipotle',
+  'Spicy Buffalo'
+]
+
+var all_chips = [
+    'Lays',
+    'Doritos'
+]
+
+var all_wings = [
+
+]
+
+var all_pita = [
+
+]
+
+var all_salad = [
+
+]
+
+const Toppings = t.enums.of(all_toppings, 'Toppings');
+const Pops = t.enums.of(all_pops, 'Pops');
+const Dips = t.enums.of(all_dips, 'Dips');
+const Chips = t.enums.of(all_chips, 'Chips');
 
 class MainModal extends React.Component {
+    handleSubmit = () => {
+      const value = this._form.getValue(); // use that ref to get the form value
+      console.log('value: ', value);  
+    }
     findItem= () => {
       this.name = this.props.navigation.state.params.name;
-      this.display_name = this.props.navigation.state.params.name;
       var category = this.props.navigation.state.params.category;
       for(var i = 0; i < custom_data[category].length; i++)
       {
@@ -25,23 +105,92 @@ class MainModal extends React.Component {
           break;
         }
       }
-        
+
+      console.log('num ', this.num_pizzas)     
       console.log('Test search: ', this.desc);
       console.log('Name: ', this.price);
     }
+    generateContent= () => {
+      if(this.num_pizzas > 0){
+        console.log('WTF')
+        this.PizzaForm = t.struct({
+          specialNotes: t.maybe(t.String),
+          toppings: t.maybe(t.list(Toppings)), 
+          toppings2: t.maybe(t.list(Toppings)),
+          toppings3: t.maybe(t.list(Toppings)),
+          pops: t.maybe(t.list(Pops)),
+          dips: t.maybe(t.list(Dips)),
+        });
+      
+        this.options = {
+          stylesheet: formStylesheet,
+          fields:{
+              specialNotes: {
+                  label: 'Special Notes (optional)',
+              },
+              toppings:{
+                  label: 'Pizza 1 Toppings',
+              },
+              toppings2:{
+                  label: 'Pizza 2 Toppings',
+              },
+              toppings3:{
+                  label: 'Pizza 3 Toppings',
+              },
+          }
+        };
+      }
+      else{
+        this.PizzaForm = t.struct({
+          specialNotes: t.maybe(t.String),
+        });
+        
+        this.options = {
+          stylesheet: formStylesheet,
+          fields:{
+              specialNotes: {
+                  label: 'Special Notes (optional)',
+              },
+            }
+          };
+      }
+    }
     render() {
       this.findItem()
+      this.generateContent()
       return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text style={{ fontSize: 30 }}>{this.display_name}</Text>
-          <Text style={{ fontSize: 30 }}></Text>
-          <Button
-            onPress={() => this.props.navigation.goBack()}
-            title="Dismiss"
-          />
-        </View>
+        <ScrollView>
+          <View style={styles.container}>
+            <Button
+              onPress={() => this.props.navigation.goBack()}
+              title="Go Back"
+            />
+            <Text style={{ fontSize: 30 }}>{this.name} ${this.price}</Text>
+            <Text style={{ fontSize: 16 }}>{this.desc}</Text>
+            <Form 
+              ref={c => this._form = c} // assign a ref
+              type={this.PizzaForm} 
+              options={this.options} // pass the options via props
+              //value={customerInfo}
+            />
+            <Button
+              title="Add to Cart"
+              onPress={this.handleSubmit}
+            />
+          </View>
+        </ScrollView>
       );
     }
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      marginTop: 50,
+      marginBottom: 150,
+      padding: 20,
+      backgroundColor: '#ffffff',
+    },
+  });
 
   export default MainModal;
