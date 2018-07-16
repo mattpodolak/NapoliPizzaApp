82d288775bc9e6bb58af7ed6702628b6c5d2d925
mattpodolak <mpodola2@uwo.ca>
2018-07-16 14:29:12 -0400

    added webview of moneris payment page

--- Modules/ThirdActivity.js
+++ Modules/ThirdActivity.js
@@ -51,6 +51,14 @@ export default class ThirdActivity extends Component{
     addtoCart= () => {
         if (this.props.navigation.state.params.cart != undefined){
             cartArr = this.props.navigation.state.params.cart;
+
+            this.subtotal = utils.totalPrice(cartArr);
+            this.delivery = utils.deliveryCost(cartArr);
+            this.tax = utils.taxCost(this.subtotal);
+            this.finalTotal = utils.finalPrice(this.subtotal, this.tax, this.delivery);
+            this.store_id = '89BCM08126'
+            this.hpp = 'hpBQQ66YSF59'
+            this.DEFAULT_URL = 'https://esqa.moneris.com/HPPDP/index.php?' + 'ps_store_id=' + this.store_id + '&hpp_key=' + this.hpp + '&charge_total=' + this.finalTotal;
         }
     }
     render(){
@@ -59,6 +67,22 @@ export default class ThirdActivity extends Component{
             <View style={{flex: 1, flexDirection: 'column'}}>
                 <View style={{height: 100}}></View>
                 <Text style={{textAlign: 'center', fontSize: 14}}>PAYMENT</Text>
+                <WebView
+                    style={{marginBottom: 20, marginTop: 20}}
+                    ref={WEBVIEW_REF}
+                    automaticallyAdjustContentInsets={false}
+                    source={{uri: this.DEFAULT_URL}}
+                    javaScriptEnabled={true}
+                    domStorageEnabled={true}
+                    decelerationRate="normal"
+                    startInLoadingState={true}
+                    scalesPageToFit={this.state.scalesPageToFit}
+                />
+                <Text style={styles.divider}></Text>
+                <Text style={styles.totals}>SUBTOTAL: {this.subtotal}</Text>
+                <Text style={styles.totals}>TAX: {this.tax}</Text>
+                <Text style={styles.totals}>DELIVERY: {this.delivery}</Text>
+                <Text style={styles.totals}>TOTAL: {this.finalTotal}</Text>  
                 <AnchorMail title="SEND ORDER"/>
             </View>
         );
@@ -66,6 +90,18 @@ export default class ThirdActivity extends Component{
 }
 // stylesheet
 const styles = StyleSheet.create({
+    divider: {
+        fontSize: 10,
+        backgroundColor: 'dimgrey',
+        margin: 7,
+        textAlign: 'center',
+        height: 4,
+    },
+    totals: {
+        fontSize: 20,
+        margin: 3,
+        textAlign: 'right',
+    },
     container: {
 		flex: 1,
 		paddingTop: StatusBar.currentHeight,
